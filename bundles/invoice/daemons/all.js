66d96670b26b36f6619f055c5d6b34ddb8604f13
Dev Server <site.support@ifactory.co>
2020-05-06 07:09:59 +1000

    remove unnecessary funtion in invoice

--- bundles/invoice/daemons/all.js
+++ bundles/invoice/daemons/all.js
@@ -73,53 +73,6 @@ class AllInvoiceDaemon extends Daemon {
       // set state approval
       invoice.set('state', 'approval');
     }
-
-    //Status Update by current docket
-    /*
-    const docket = await ((await invoice.get('orders')).filter(async (order, i) => i === 0))[0].get('docket');
-    docket && docket.get('status') ? docket.get('status') === 'complete' ? invoice.set('status', 'complete') : docket.get('status') === 'en-route' ? invoice.set('status', 'delivery') : invoice.set('status', 'draft') : invoice.set('status', 'draft');
-    */
-  }
-  /**
-   * pre invoice update
-   *
-   * @param  {payment} Payment
-   *
-   * @pre payment.update
-   * @pre payment.create
-   */
-  async invoiceUpdatebyPaymentHook(payment) {
-    /*
-    console.log('invoiceUpdatebyPaymentHook');
-    const invoice = payment.get('invoice') ? await payment.get('invoice') : '';
-    if (!invoice) return ;
-    const invoiceamount = invoice.get('total');
-    let amount = 0;
-    let state  = [];
-
-    if (payment.get('state') && payment.get('state') !== 'cancel' && payment.get('state') !== 'refund') {
-      parseFloat(payment.get('amount')) > 0 ? state.push(payment.get('state')) : '';
-      if (payment.get('state') === 'paid') {
-        amount += parseFloat(payment.get('amount'));
-      }
-    }
-
-    const payments = await Payment.where({ 'invoice.id' : invoice.get('_id') }).find();
-
-    if (payments.length > 0) payments.map(p => {
-      if ((!payment.get('_id') || p.get('_id') !== payment.get('_id')) && p.get('state') !== 'cancel' && p.get('state') !== 'refund') {
-        parseFloat(p.get('amount')) > 0 ? state.push(p.get('state')) : '';
-        if (p.get('state') === 'paid') {
-          amount += parseFloat(p.get('amount'));
-        }
-      }
-    });
-
-    invoice.set('state', (amount === invoiceamount && invoiceamount > 0) ? 'paid' : state.length > 0 ? 'approval' : 'unpaid');
-    invoice.set('remain', (amount === invoiceamount && invoiceamount > 0) ? 0 : (invoiceamount-amount));
-
-    await invoice.save();
-    */
   }
 }
 
